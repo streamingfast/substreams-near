e21ca9982afc9258f3fa4761d95dffa2a4bbffeb
colindickson <colin.dickson@gmail.com>
2023-07-19 09:04:28 -0400

    prepare 0.9.6 release

--- core/src/block_view.rs
+++ core/src/block_view.rs
@@ -1,9 +1,22 @@
 use crate::{pb::sf::near::r#type::v1 as pb};
+use hex;
 
 impl pb::Block {
     pub fn state_changes(&self) -> StateChangesView<'_> {
         StateChangesView {
             state_changes: &self.state_changes,
+        }
+    }
+
+    pub fn hash(&self) -> Option<String> {
+        match &self.header {
+            Some(header) => {
+                match &header.hash {
+                    Some(hash) => Some(hex::encode(&hash.bytes)),
+                    None => None,
+                }
+            },
+            None => None,
         }
     }
 }
